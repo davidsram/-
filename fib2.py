05c9d90f202094c648da1811ef0fa441a724028d
davidsram <798014863@qq.com>
2018-11-06 10:20:51 +0800

    second

--- fib2.py
+++ fib2.py
@@ -1,8 +1,13 @@
 def fib(max):
     n, a, b = 0, 0, 1
+    c=[]
     while n < max:
         print (b)
+        c.append(b)
         a, b = b, a + b
         n = n + 1
+
+    print(c)
+    print(sum(c))
     return 'done'
 fib(6)
